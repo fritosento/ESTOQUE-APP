652a61366e99d77ef1cc5340d11a9bcdb1548180
rivaldo <rivaldocamposbr@gmail.com>
2025-10-30 09:53:54 -0300

    funções de status e icons

--- src/Main/index.js
+++ src/Main/index.js
@@ -5,10 +5,27 @@ import Products from "../components/Products";
 import { products } from "../mocks/Products";
 
 export default function Main() {
+  function handleDeleteProduct(id) {
+    alert(`Sem funcionalidade ${id}`);
+  }
+
+  function handleEditProduct(product) {
+    alert(`Sem funcionalidade ${product.id}`);
+  }
+
+  function handleChangeStatus(id) {
+    alert(`Sem funcionalidade ${id}`);
+  }
+
   return (
     <Container>
       <Header />
-      <Products products={products} />
+      <Products
+        products={products}
+        onChangeStatus={handleChangeStatus}
+        onDelete={handleDeleteProduct}
+        onEditproducts={handleEditProduct}
+      />
     </Container>
   );
 }
